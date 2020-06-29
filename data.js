8ec119bc95c88bb509dce625d187ede2409b857c
edsonBJR <ebjr.tux@gmail.com>
2020-06-29 15:56:34 -0300

    Todos arquivos alterados para Nunjucks

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = [{
+        id: "starter",
+        courseNumber: "01",
+        image: "https://skylab.rocketseat.com.br/static/media/Starter.64c237cc.svg",
+        description01: "Torne se um programador desejado no mercado com esses cursos gratuitos"
+    },
+    {
+        id: "launchBase",
+        courseNumber: "02",
+        image: "https://skylab.rocketseat.com.br/static/media/LaunchBase.08285320.svg",
+        description01: "Domine programação do zero e tenha acesso às melhores oportunidades do mercado"
+    },
+    {
+        id: "goStack",
+        courseNumber: "03",
+        image: "https://skylab.rocketseat.com.br/static/media/GoStack.83a178a0.svg",
+        description01: "Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile"
+    }
+]
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+
+const courses = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res) {
+    return res.render("home")
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+})
+
+server.get("/courses", function(req, res) {
+
+    return res.render("courses", { items: courses })
+})
+
+server.get("/courses/:id", function(req, res) {
+    const id = req.params.id;
+
+    return res.send(`O id fornecido na rota é: ${id}`)
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found")
+})
+
+server.listen(5000, function() {
+    console.log("Server is runing...")
+})
